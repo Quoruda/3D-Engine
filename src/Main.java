f0074a3c76f562ea8b94a1d583b4349141a7f72a
Quoruda <audrick.soltner@proton.me>
2024-10-29 00:53:44 +0100

    Remove tests

--- src/Main.java
+++ src/Main.java
@@ -7,64 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Engine engine = new Engine();
 
-        /*
-        Mesh sphere = Mesh.loadFromObjectFile("models/sphere.obj");
-        sphere.setPosition(0, 0, 0);
-        Mesh cube = Mesh.loadFromObjectFile("models/cube.obj");
-        cube.setPosition(5, 0, 0);
-        Mesh cube2 = Mesh.loadFromObjectFile("models/cube.obj");
-        cube2.setPosition(-5, 0, 0);
-        Mesh cube3 = Mesh.loadFromObjectFile("models/cube.obj");
-        cube3.setPosition(0, 0, 5);
-        Mesh cube4 = Mesh.loadFromObjectFile("models/cube.obj");
-        cube4.setPosition(0, 0, -5);
-
-         */
-        /*
-        Mesh cube = new Mesh();
-        cube.vertices = new float[][]{
-                {0.0f, 0.0f, 0.0f},//0
-                {1.0f, 0.0f, 0.0f},//1
-                {1.0f, 1.0f, 0.0f},//2
-                {0.0f, 1.0f, 0.0f},//3
-                {0.0f, 0.0f, 1.0f},//4
-                {1.0f, 0.0f, 1.0f},//5
-                {1.0f, 1.0f, 1.0f},//6
-                {0.0f, 1.0f, 1.0f}//7
-        };
-
-        cube.triangles = new int[][]{
-                {0, 3, 2},
-                {0, 2, 1},
-                {1, 2, 6},
-                {1, 6, 5},
-                {5, 6, 7},
-                {5, 7, 4},
-                {4, 7, 3},
-                {4, 3, 0},
-                {3, 7, 6},
-                {3, 6, 2},
-                {5, 4, 0},
-                {5, 0, 1}
-        };
-
-        cube.tList = new float[][][]{
-                {{0, 1, 1}, {0, 0, 1}, {1, 0, 1}},
-                {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}},
-                {{0, 1, 1}, {0, 0, 1}, {1, 0, 1}},
-                {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}},
-                {{0, 1, 1}, {0, 0, 1}, {1, 0, 1}},
-                {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}},
-                {{0, 1, 1}, {0, 0, 1}, {1, 0, 1}},
-                {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}},
-                {{0, 1, 1}, {0, 0, 1}, {1, 0, 1}},
-                {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}},
-                {{0, 1, 1}, {0, 0, 1}, {1, 0, 1}},
-                {{0, 1, 1}, {1, 0, 1}, {1, 1, 1}}
-        };
-
-         */
-
         Mesh cube = Mesh.loadFromObjectFile("models/cube.obj", true);
         cube.setTexture(Texture.getGrid(256, 256, 16, 0xFF0000, 0x00FF00));
 
