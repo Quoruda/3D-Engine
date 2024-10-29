ab4c8006c649ebb226fefee3aa729a9440d86677
Quoruda <audrick.soltner@proton.me>
2024-10-29 16:19:59 +0100

    Update camera features

--- src/Camera.java
+++ src/Camera.java
@@ -3,12 +3,14 @@ public class Camera {
 
     public float[] vCamera, vLookDir;
     float fYaw;
+    float clipped_distance;
 
 
     public Camera(){
         vCamera = new float[]{0,0,0,1};
         vLookDir = new float[]{0,0,0,1};
         fYaw = 0;
+        clipped_distance = 0.1f;
     }
 
     public void setPosition(float x, float y, float z){
@@ -22,6 +24,18 @@ public class Camera {
         float[] vTarget = Geometry.VectorAddition(this.vCamera, vLookDir);
         float[][] matCamera = Geometry.Matrix_PointAt(this.vCamera, vTarget, vUp);
         return matCamera;
+    }
+
+    public void setYawFromDegrees(int degrees){
+        this.fYaw = degrees * (float) Math.PI / 180f;
+    }
+
+    public void translateY(float v){
+        this.vCamera[1] += v;
+    }
+
+    public void translateX(float v){
+        this.vCamera[0] += v;
     }
 
 }
