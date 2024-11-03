7e45740f85fe42e0689adac323b2a9baa50b7e9b
Quoruda <audrick.soltner@proton.me>
2024-11-04 00:22:25 +0100

    Add loading option

--- src/Objects/Texture.java
+++ src/Objects/Texture.java
@@ -17,7 +17,7 @@ public class Texture {
     }
 
     public int getPixel(float u, float v) {
-        return pixels[(int)(u * width) + (int)(v * height) * width];
+        return pixels[(int)(u * (width-1)) + (int)(v * (height-1)) * width];
 
         //return pixels[Math.floorMod((int)(u * this.width-1) + (int)((1f-v) * (this.height-1)) * (this.width-1), pixels.length)];
     }
@@ -47,25 +47,27 @@ public class Texture {
     }
 
     public static Texture loadFromFile(String fileName){
-        Texture texture = new Texture();
         try {
             BufferedImage image = ImageIO.read(new File(fileName));
-            int width = image.getWidth();
-            int height = image.getHeight();
-            texture.height = height;
-            texture.width = width;
-            texture.pixels = new int[height*width];
-
-            for(int x = 0; x < width; x++){
-                for(int y = 0; y < height; y++){
-                    int pixel = image.getRGB(x,y);
-                    texture.pixels[(height-y-1)*width+x] = pixel;
-                }
-            }
-
-
+            return loadFromBufferedImage(image);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static Texture loadFromBufferedImage(BufferedImage image){
+        Texture texture = new Texture();
+        int width = image.getWidth();
+        int height = image.getHeight();
+        texture.height = height;
+        texture.width = width;
+        texture.pixels = new int[height*width];
+
+        for(int x = 0; x < width; x++){
+            for(int y = 0; y < height; y++){
+                int pixel = image.getRGB(x,y);
+                texture.pixels[(height-y-1)*width+x] = pixel;
+            }
         }
         return texture;
     }
